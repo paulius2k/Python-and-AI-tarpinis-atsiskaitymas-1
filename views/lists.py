4b1fd435a46c76dc8899bca623381bdac07267dd
Paulius Kriščiūnas <paulius2k@gmail.com>
2024-12-12 14:33:03 +0200

    entered some new items into library database

--- views/lists.py
+++ views/lists.py
@@ -9,27 +9,24 @@ def list_catalogue_dynamic(data: Catalogue, scope_msg = ""):
     selected_item_id = None
     
     try:
-        big_separator = f"{"=" * 145}"
-        small_separator = f"{"-" * 145}"
-        
-        col_width = 30
-        col_width_narrow = 8
-        
-        header = f"{"Eil.nr":<{col_width_narrow}}{"Title":<{col_width}}{"Author":<{col_width}}{"Genre":<{col_width}}{"Year":<{col_width_narrow}}{"Pcs.total:":>{col_width_narrow}}{"availbl:":>{col_width_narrow}}"
 
-        inquirer_msg_text = f"Keys: ↑, ↓ - to move, ENTER - to choose action with item, Esc - to exit\n"        
-        inquirer_msg_text += f"\n  {scope_msg}\n"
-        
         if len(data) == 0:
             display_text = f"\n  No items found.\n"
             print(display_text)
             wait_for_keypress = input("Press ENTER to continue...")
             
         else:
-            # print(big_separator)
-            # print(header)
-            # print(small_separator)
-            inquirer_msg_text += f"  {big_separator}\n  {header}\n  {small_separator}\n"
+            big_separator = f"{"=" * 145}"
+            small_separator = f"{"-" * 145}"
+            
+            col_width = 30
+            col_width_narrow = 9
+            
+            header = f"{"Eil.nr":<{col_width_narrow}}{"Title":<{col_width}}{"Author":<{col_width}}{"Genre":<{col_width}}{"Year":<{col_width_narrow}}{"Pcs.total":>{col_width_narrow}}{"/availbl:":>{col_width_narrow}}"
+
+            inquirer_msg_text = f"Keys: ↑, ↓ - to move, ENTER - to choose action with item, Esc - to exit\n"        
+            inquirer_msg_text += f"\n  {scope_msg} (found: {len(data)})\n"
+            inquirer_msg_text += f"  {big_separator}\n  {header}\n  {small_separator}"
             
             for idx, item in enumerate(data, start=1):
                 if len(item.title) >= col_width:
@@ -42,7 +39,7 @@ def list_catalogue_dynamic(data: Catalogue, scope_msg = ""):
                 else:
                     author_checked = item.author
                 
-                item_line = f" {idx:<{col_width_narrow}}{title_checked:<{col_width}}{author_checked:<{col_width}}{item.genre:<{col_width}}{item.publication_year:<{col_width_narrow}}{item.total_units:>{col_width_narrow}}{item.available_units:>{col_width_narrow}}"
+                item_line = f"{idx:<{col_width_narrow}}{title_checked:<{col_width}}{author_checked:<{col_width}}{item.genre:<{col_width}}{item.publication_year:<{col_width_narrow}}{item.total_units:>{col_width_narrow}}{item.available_units:>{col_width_narrow}}"
                 items_list.append(Choice(value=item.id, name=item_line))   
         
             selected_item_id = inquirer.select(
